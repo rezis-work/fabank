b9f92850818fcbf82233d2ffd28b01e7699839d0
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-05-07 22:57:55 +0400

    implement complete sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Rezi" };
+  const loggedIn = {
+    firstName: "Rezi",
+    lastName: "Karanadze",
+    email: "rkrkaranadze@gmail.com",
+  };
   return (
     <section className="home">
       <div className=" home-content">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentTransaction = () => {
+  return <div>PaymentTransaction</div>;
+};
+
+export default PaymentTransaction;
