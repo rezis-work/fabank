6946a1a0e3889dbca55c9c2bb8bf310c9364ee4e
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-05-12 18:29:11 +0400

    working on bug

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,4 +1,5 @@
 "use client";
+// CustomInput.js
 import React from "react";
 import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
 import { Input } from "./ui/input";
@@ -27,18 +28,18 @@ const CustomInput = ({
       control={control}
       name={name}
       render={({ field }) => (
-        <div className=" form-item">
-          <FormLabel className=" form-label">{label}</FormLabel>
-          <div className=" flex w-full flex-col">
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
             <FormControl>
               <Input
                 placeholder={placeholder}
-                className=" input-class"
+                className="input-class"
                 type={name === "password" ? "password" : "text"}
                 {...field}
               />
             </FormControl>
-            <FormMessage className=" form-message mt-2" />
+            <FormMessage className="form-message mt-2" />
           </div>
         </div>
       )}
